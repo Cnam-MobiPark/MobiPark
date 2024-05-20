d0e04c1ed912203b7f0c63a82ddc7426e17c8207
Bastian <badmaxyt@gmail.com>
2024-05-20 23:22:08 +0200

    feat: :sparkles: add id attribute in Vehicle

--- backend/MobiPark.App/Presenters/VehiclePresenter.cs
+++ backend/MobiPark.App/Presenters/VehiclePresenter.cs
@@ -4,12 +4,14 @@ namespace MobiPark.App.Presenters;
 
 public class VehiclePresenter
 {
+    public int Id { get; private set; }
     public Vehicle.VehicleType Type { get; private set; }
     public string Maker { get; private set; }
     public string LicensePlate { get; private set; }
     
     public VehiclePresenter(Vehicle vehicle)
     {
+        Id = vehicle.Id;
         Type = vehicle.Type;
         Maker = vehicle.Maker;
         LicensePlate = vehicle.LicensePlate;
--- backend/MobiPark.Domain/Models/Vehicle.cs
+++ backend/MobiPark.Domain/Models/Vehicle.cs
@@ -7,6 +7,7 @@ namespace MobiPark.Domain.Models
             Car,
             Motorcycle
         }
+        public int Id { get; set; }
         public VehicleType Type { get; set; }
         public string Maker { get; set; }
         public string LicensePlate { get; set; }
