ded85ff22563fb05834e191ec886c3a6ad3c0225
Bastian <badmaxyt@gmail.com>
2024-05-22 14:52:23 +0200

    test: :test_tube: add CreateVehicle_ShouldCreateVehicle failing test

--- backend/MobiPark.Domain.Test/VehicleTest.cs
+++ backend/MobiPark.Domain.Test/VehicleTest.cs
@@ -1,9 +1,37 @@
+using MobiPark.Domain.Models;
 using MobiPark.Domain.Services;
 
 namespace MobiPark.Domain.Test;
 
 public class VehicleTest
 {
+    
+    public VehicleTest()
+    {
+        var vehicleRepository = new VehicleRepository();
+        _vehicleService = new VehicleService(vehicleRepository);
+    }
+    
+    [Fact]
+    [Trait("Category", "Creating Vehicles")]
+    public void CreateVehicle_ShouldCreateVehicle()
+    {
+        // Arrange
+        var type = Vehicle.VehicleType.Car;
+        var maker = "Toyota";
+        var licensePlate = "ABC123";
+        
+        // Act
+        var vehicle = _vehicleService.CreateVehicle(type, maker, licensePlate);
+        
+        // Assert
+        Assert.NotNull(vehicle);
+        Assert.Equal(type, vehicle.Type);
+        Assert.Equal(maker, vehicle.Maker);
+        Assert.Equal(licensePlate, vehicle.LicensePlate);
+    }
+    
+    
     [Fact]
     [Trait("Category", "Creating Vehicles")]
     public void GetVehicles_ReturnsVehicles()
