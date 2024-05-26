8e28edfe52c7f5359f0d132673895a0cf3f57a66
Bastian <badmaxyt@gmail.com>
2024-05-26 11:56:48 +0200

    test: :test_tube: add failing test for LicensePlates verification

--- backend/MobiPark.Domain.Test/VehicleTest.cs
+++ backend/MobiPark.Domain.Test/VehicleTest.cs
@@ -1,3 +1,4 @@
+using MobiPark.Domain.Exceptions;
 using MobiPark.Domain.Models.Vehicle;
 using MobiPark.Domain.Services;
 using MobiPark.Domain.Test.Repository;
@@ -22,6 +23,90 @@ public class VehicleTest
         // Arrange
         var maker = "Toyota";
         var licensePlate = new FrLicensePlate("AB-123-CD");
+
+        // Act
+        var vehicle = VehicleFactory.CreateCar(maker, licensePlate);
+
+        // Assert
+        Assert.NotNull(vehicle);
+        Assert.IsType<Car>(vehicle);
+        Assert.Equal(maker, vehicle.Maker);
+        Assert.Equal(licensePlate, vehicle.LicensePlate);
+    }
+    
+    [Fact]
+    [Trait("Category", "Creating Vehicles")]
+    public void CreateVehicle_ShouldThrowException_WhenLicensePlateIsNull()
+    {
+        // Arrange
+        var maker = "Toyota";
+
+        // Act
+        Action act = () => VehicleFactory.CreateCar(maker, new FrLicensePlate("null"));
+
+        // Assert
+        var exception = Assert.Throws<InvalidLicensePlateException>(act);
+        Assert.Equal("Invalid license plate: null", exception.Message);
+        
+    }
+    
+    [Fact]
+    [Trait("Category", "Creating Vehicles")]
+    public void CreateVehicle_ShouldThrowException_WhenLicensePlateIsInvalidforFr()
+    {
+        // Arrange
+        var maker = "Toyota";
+        var invalidLicensePlate = "AB-123-CD-123";
+
+        // Act
+        Action act = () => VehicleFactory.CreateCar(maker, new FrLicensePlate(invalidLicensePlate));
+
+        // Assert
+        var exception = Assert.Throws<InvalidLicensePlateException>(act);
+        Assert.Equal("Invalid license plate: " + invalidLicensePlate, exception.Message);
+    }
+    
+    [Fact]
+    [Trait("Category", "Creating Vehicles")]
+    public void CreateVehicle_ShouldWork_WhenLicensePlateIsValidforFr()
+    {
+        // Arrange
+        var maker = "Toyota";
+        var licensePlate = new FrLicensePlate("AB-123-CD");
+
+        // Act
+        var vehicle = VehicleFactory.CreateCar(maker, licensePlate);
+
+        // Assert
+        Assert.NotNull(vehicle);
+        Assert.IsType<Car>(vehicle);
+        Assert.Equal(maker, vehicle.Maker);
+        Assert.Equal(licensePlate, vehicle.LicensePlate);
+    }
+    
+    [Fact]
+    [Trait("Category", "Creating Vehicles")]
+    public void CreateVehicle_ShouldThrowException_WhenLicensePlateIsInvalidforDe()
+    {
+        // Arrange
+        var maker = "Toyota";
+        var invalidLicensePlate = "AB-123-CD";
+        
+        // Act
+        Action act = () => VehicleFactory.CreateCar(maker, new DeLicensePlate(invalidLicensePlate));
+        
+        // Assert
+        var exception = Assert.Throws<InvalidLicensePlateException>(act);
+        Assert.Equal("Invalid license plate: " + invalidLicensePlate, exception.Message);
+    }
+    
+    [Fact]
+    [Trait("Category", "Creating Vehicles")]
+    public void CreateVehicle_ShouldWork_WhenLicensePlateIsValidforDe()
+    {
+        // Arrange
+        var maker = "Toyota";
+        var licensePlate = new DeLicensePlate("A X 1234");
 
         // Act
         var vehicle = VehicleFactory.CreateCar(maker, licensePlate);
