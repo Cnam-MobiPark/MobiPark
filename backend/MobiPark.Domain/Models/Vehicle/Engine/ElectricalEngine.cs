cae25bbc50a24c94a703cc24f0eb08e5978263d2
Bastian <badmaxyt@gmail.com>
2024-05-29 22:04:32 +0200

    feat: :white_check_mark: pass test by handling exceptions

--- backend/MobiPark.Domain/Models/Vehicle/Engine/ElectricalEngine.cs
+++ backend/MobiPark.Domain/Models/Vehicle/Engine/ElectricalEngine.cs
@@ -1,3 +1,5 @@
+using MobiPark.Domain.Exceptions;
+
 namespace MobiPark.Domain.Models.Vehicle.Engine
 {
     public class ElectricalEngine : Engine
@@ -8,6 +10,20 @@ namespace MobiPark.Domain.Models.Vehicle.Engine
     
         public ElectricalEngine(int batteryCapacity, int currentBatteryCapacity)
         {
+            if (batteryCapacity <= 0)
+            {
+                throw new InvalidBatteryException("Battery capacity : " + batteryCapacity + " cannot be negative or zero.");
+            }
+            
+            if (currentBatteryCapacity < 0)
+            {
+                throw new InvalidBatteryException("Current battery capacity : " + currentBatteryCapacity + " cannot be negative.");
+            }
+            if (currentBatteryCapacity > batteryCapacity)
+            {
+                throw new InvalidBatteryException("Current battery capacity : " + currentBatteryCapacity + " cannot be greater than battery capacity : " + batteryCapacity);
+            }
+            
             this.batteryCapacity = batteryCapacity;
             this.currentBatteryCapacity = currentBatteryCapacity;
         }
