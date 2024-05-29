cc0bef1f77b923a700af555b5576236a6a2d80e3
Bastian <badmaxyt@gmail.com>
2024-05-29 22:49:07 +0200

    refactor: :fire: remove CreateVehicle method

--- backend/MobiPark.Domain/Services/VehicleService.cs
+++ backend/MobiPark.Domain/Services/VehicleService.cs
@@ -1,7 +1,5 @@
 using MobiPark.Domain.Interfaces;
 using MobiPark.Domain.Models.Vehicle;
-using MobiPark.Domain.Models.Vehicle.Engine;
-using MobiPark.Domain.Models.Vehicle.LicensePlate;
 
 namespace MobiPark.Domain.Services
 {
@@ -12,11 +10,6 @@ namespace MobiPark.Domain.Services
         public VehicleService(IVehicleRepository repository)
         {
             _repository = repository;
-        }
-        
-        public Vehicle CreateVehicle(string type, string maker, AbstractLicensePlate licensePlate, Engine engine)
-        {
-            return _repository.CreateVehicle(type, maker, licensePlate, engine);
         }
         
 
--- backend/MobiPark.Domain/interfaces/IVehicleService.cs
+++ backend/MobiPark.Domain/interfaces/IVehicleService.cs
@@ -1,12 +1,9 @@
 using MobiPark.Domain.Models.Vehicle;
-using MobiPark.Domain.Models.Vehicle.Engine;
-using MobiPark.Domain.Models.Vehicle.LicensePlate;
 
 namespace MobiPark.Domain.Interfaces
 {
     public interface IVehicleService
     {
-        Vehicle CreateVehicle(string type, string maker, AbstractLicensePlate licensePlate, Engine engine);
         List<Vehicle> GetVehicles();
     }
 }
