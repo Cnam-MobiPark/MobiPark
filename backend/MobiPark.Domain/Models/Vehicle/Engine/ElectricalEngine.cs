b4503c2a842f60ed5719e8f56b18b0b351e24314
Bastian <badmaxyt@gmail.com>
2024-06-02 19:20:48 +0200

    refactor: :recycle: edit attributes

--- backend/MobiPark.Domain/Models/Vehicle/Engine/ElectricalEngine.cs
+++ backend/MobiPark.Domain/Models/Vehicle/Engine/ElectricalEngine.cs
@@ -4,28 +4,31 @@ namespace MobiPark.Domain.Models.Vehicle.Engine
 {
     public class ElectricalEngine : Engine
     {
-        public readonly int batteryCapacity;
-        public int currentBatteryCapacity { get; private set; }
-        public int batteryLevel => (currentBatteryCapacity * 100) / batteryCapacity;
-    
+        public int BatteryCapacity { get; private set; }
+        public int CurrentBatteryCapacity { get; private set; }
+        public int BatteryLevel => (CurrentBatteryCapacity * 100) / BatteryCapacity;
+
         public ElectricalEngine(int batteryCapacity, int currentBatteryCapacity)
         {
             if (batteryCapacity <= 0)
             {
-                throw new InvalidBatteryException("Battery capacity : " + batteryCapacity + " cannot be negative or zero.");
+                throw new InvalidBatteryException("Battery capacity cannot be negative or zero.");
             }
-            
+
             if (currentBatteryCapacity < 0)
             {
-                throw new InvalidBatteryException("Current battery capacity : " + currentBatteryCapacity + " cannot be negative.");
+                throw new InvalidBatteryException("Current battery capacity cannot be negative.");
             }
             if (currentBatteryCapacity > batteryCapacity)
             {
-                throw new InvalidBatteryException("Current battery capacity : " + currentBatteryCapacity + " cannot be greater than battery capacity : " + batteryCapacity);
+                throw new InvalidBatteryException("Current battery capacity cannot be greater than battery capacity.");
             }
-            
-            this.batteryCapacity = batteryCapacity;
-            this.currentBatteryCapacity = currentBatteryCapacity;
+
+            BatteryCapacity = batteryCapacity;
+            CurrentBatteryCapacity = currentBatteryCapacity;
         }
+
+        // Parameterless constructor for EF
+        private ElectricalEngine() { }
     }
 }
