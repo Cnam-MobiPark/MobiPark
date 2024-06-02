f23c1eaeb3105c3a932b18ef9cef56b45db178cc
Louis Bailleau <louis13.bailleau@gmail.com>
2024-06-02 15:37:33 +0200

    feat: add login page UI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from './guards/guest.guard';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    canActivate: [guestGuard],
+    loadComponent: () =>
+      import('./pages/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [],
+  },
+];
