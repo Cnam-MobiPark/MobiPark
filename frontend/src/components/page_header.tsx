80d3b812b02da36c68020d2542939b85879ec2c7
Louis Bailleau <louis13.bailleau@gmail.com>
2024-06-03 13:59:12 +0200

    feat: add reservation page UI

--- frontend/src/components/page_header.tsx
+++ frontend/src/components/page_header.tsx
@@ -10,7 +10,7 @@ export function PageHeader({
   description,
 }: PageHeaderProps): ReactElement {
   return (
-    <header>
+    <header className="mb-8">
       <h1 className="text-4xl font-bold mb-2">{title}</h1>
 
       {description && <p className="text-muted-foreground">{description}</p>}
--- frontend/src/pages/user/home.tsx
+++ frontend/src/pages/user/home.tsx
@@ -1,13 +1,178 @@
 import { type ReactElement } from "react";
 import { PageHeader } from "../../components/page_header";
+import { useForm } from "react-hook-form";
+import { format } from "date-fns";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Calendar } from "@/components/ui/calendar";
+import { z } from "zod";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { CalendarIcon } from "lucide-react";
+
+const formSchema = z.object({
+  vehicle: z.string(),
+  licencePlate: z.string(),
+  beginDate: z.date(),
+  endDate: z.date(),
+});
 
 export function UserHome(): ReactElement {
+  const form = useForm<z.infer<typeof formSchema>>();
+
+  function onSubmit() {}
+
   return (
     <div>
       <PageHeader
         title="Faire une réservation"
         description="Réserver une place de parking pour votre véhicule"
       />
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="vehicle"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Véhicule</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Sélectionnez un véhicule" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent></SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="licencePlate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Immatriculation</FormLabel>
+                <FormControl>
+                  <Input placeholder="Immatriculation" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="beginDate"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Du</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="beginDate"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Au</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 }
