b6551031814733d0eb05dd8e39f9f4e56ba5b2d7
Bastian <badmaxyt@gmail.com>
2024-06-04 15:53:49 +0200

    feat: :card_file_box: rework migrations and db

--- backend/MobiPark.Infra/Migrations/20240604130751_initCreate.Designer.cs
+++ backend/MobiPark.Infra/Migrations/20240604130751_initCreate.Designer.cs
@@ -10,7 +10,7 @@ using MobiPark.Infra;
 namespace MobiPark.Infra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240604082405_initCreate")]
+    [Migration("20240604130751_initCreate")]
     partial class initCreate
     {
         /// <inheritdoc />
@@ -25,13 +25,13 @@ namespace MobiPark.Infra.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
 
+                    b.Property<bool>("IsElectric")
+                        .HasColumnType("INTEGER");
+
                     b.Property<int>("Size")
                         .HasColumnType("INTEGER");
 
                     b.Property<int>("Status")
-                        .HasColumnType("INTEGER");
-
-                    b.Property<bool>("isElectric")
                         .HasColumnType("INTEGER");
 
                     b.HasKey("Number");
@@ -60,6 +60,25 @@ namespace MobiPark.Infra.Migrations
                         .IsUnique();
 
                     b.ToTable("Reservations");
+                });
+
+            modelBuilder.Entity("MobiPark.Infra.Entities.UserEntity", b =>
+                {
+                    b.Property<int>("UserId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Password")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Username")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("UserId");
+
+                    b.ToTable("Users");
                 });
 
             modelBuilder.Entity("MobiPark.Infra.Entities.VehicleEntity", b =>
